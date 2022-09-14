01cf008a384d3461102cd511ec99099141c7e668
jorgebarcelos <jorgebarcelos.faria@gmail.com>
2022-09-13 21:34:25 -0300

    aplication updated

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
+import email
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField('nome', max_length=100)
+    preco = models.DateField('preço')
+    estoque = models.IntegerField('qtd em estoque')
+
+
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
+from multiprocessing import context
+import re
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'curso': 'Zé da Couve'
+    }
+    return render(request, 'index.html', context)
+
+
+def contato(request):
+    return render(request, 'contato.html')
